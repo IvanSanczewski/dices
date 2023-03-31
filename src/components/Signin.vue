832db10df364c5a975ee8bdcc68bc6182cddd110
IvanSanczewski <isanczewski@gmail.com>
2023-03-31 17:40:17 +0300

    Created SignIn & LogIn components

---
+++ src/components/Signin.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>This is the Sign In Component</p>
+</template>
+
+<script setup>
+import { useDicesStore } from "@/stores/dices"
+
+const storeDices = useDicesStore ()
+
+
+</script>
