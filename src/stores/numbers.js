70ff6aa2f8bf601cf5527dc5f1f7159164a506db
IvanSanczewski <isanczewski@gmail.com>
2023-04-24 23:06:30 +0300

    Implemented Fisher-Yates algo to populate the cells of the square array, each with a unique number from 1 to the total square cells

--- src/stores/numbers.js
+++ src/stores/numbers.js
@@ -3,13 +3,30 @@ import { defineStore } from 'pinia'
 export const useNumbersStore = defineStore('numbers', {
     state: () => ({
         //big square
-        bigSquare:'',
-        actualSquare: ''
+        squareSize: null,
+        squareValues: []
+
     }),
 
     actions: {
         setSquare(side) {
-            this.bigSquare = side * side
+            console.log(side);
+            this.squareSize = side * side
+            console.log(this.squareSize);
+        },
+
+        // generate a different value for each cell of the square using the Fisher-Yates algo
+        generateSquare() {
+            // first we populate the array from 1 to the length of the square for each index
+            this.squareValues = Array.from({length: this.squareSize}, (_, i) => i + 1);
+
+            // then, starting from the end, in each iteration we generate a random number between 0 and the current iteration
+            this.squareValues.forEach((_, i, a) => {
+              const j = Math.floor(Math.random() * (i + 1));
+              // finally we swap these two values
+              [a[i], a[j]] = [a[j], a[i]];
+            });
         }
     }
 })
+
--- src/views/Numbers.vue
+++ src/views/Numbers.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="container">
-        <h1>This is NUMBERS game</h1>
-    </div>
+    <div class="numbers-game">
+        <div class="container">
+            <h1>This is NUMBERS game</h1>
+        </div>
+        <div class="choose-level">
+            <h2>How many squares you want to play?</h2>
+                <button class="numbers-btn" @click="storeNumbers.setSquare(3)">3<sup>2</sup></button>
+                <button class="numbers-btn" @click="storeNumbers.setSquare(5)">5<sup>2</sup></button>
+                <button class="numbers-btn" @click="storeNumbers.setSquare(7)">7<sup>2</sup></button>
+        </div>
+        <div v-if="storeNumbers.squareSize !== null" class="play-numbers">
+            <button @click="storeNumbers.generateSquare" class="numbers-btn">PLAY NUMBERS!</button>
+        </div>
 
-    <div class="choose-level">
-        <h2>How many squares you want to play?</h2>
-        <p>3<sup>2</sup></p>
-        <p>5<sup>2</sup></p>
-        <p>7<sup>2</sup></p>
-        
+        <div class="square">
+            <div v-for="cell, index in storeNumbers.squareValues" :key="index" class="cell">
+                {{ cell }}
+            </div>
+        </div>
     </div>
 </template>
 
