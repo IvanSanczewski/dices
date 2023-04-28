c22830ec49a45f84f9043b5ec4e209e496b2eb20
IvanSanczewski <isanczewski@gmail.com>
2023-04-28 22:59:53 +0300

    Implemented sign in form and user state

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,11 +1,19 @@
 <template>
     <p>This is the Sign In Component</p>
+
+    <form @submit.prevent="storeUsers.signin">
+        <input type="name" placeholder="Name" v-model="storeUsers.user.name" required>
+        <input type="email" placeholder="Email" v-model="storeUsers.user.email" required>
+        <input type="password" placeholder="Password" v-model="storeUsers.user.password" required>
+        <button>SIGN IN</button>
+    </form>
 </template>
 
 <script setup>
 import { useDicesStore } from "@/stores/dices"
+import { useUsersStore } from "@/stores/users";
 
-const storeDices = useDicesStore ()
-
+const storeDices = useDicesStore()
+const storeUsers = useUsersStore()
 
 </script>
--- src/stores/users.js
+++ src/stores/users.js
@@ -4,9 +4,22 @@ export const useUsersStore = defineStore('users', {
     state: () => ({
         // display
         isLogged: false,
+        
+        // user data
+        user:{
+            name: '',
+            email:'',
+            password:''
+        }
     }),
 
     actions: {
+        signin() {
+            this.toggleLogged()
+            console.log('SIGNED IN');
+            
+        },
+        
         toggleLogged() {
             this.isLogged = !this.isLogged
         }
