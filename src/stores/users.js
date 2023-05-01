f89e60698f9c7e873e3fba347a2dda23c2acfff1
Ivan Sanczewski <isanczewski@gmail.com>
2023-05-01 20:18:29 +0300

    Comments on user Sign In method

--- src/stores/users.js
+++ src/stores/users.js
@@ -17,14 +17,19 @@ export const useUsersStore = defineStore('users', {
         signinTry() {
             this.signin()
         },
-        
+        // function called on submit 
         signin() {
             console.log(this.user.name, this.user.email, this.user.password)
             
+            // in try block we send user sign in info to FIREBASE
+            // promise
             try {
+
+                // after signing in, call toggle function to show logged user name
                 this.toggleLogged()
                 console.log('SIGNED IN');
-                
+            
+            // throw error if connection to FIREBASE failed
             } catch (error) {
                 
             }
