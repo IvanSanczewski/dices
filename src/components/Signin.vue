7318d9710a358f2ca62e6c63fc51305b3e942dbe
IvanSanczewski <isanczewski@gmail.com>
2023-05-04 23:04:36 +0300

    Introduced refs at sign in

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -2,9 +2,14 @@
     <p>This is the Sign In Component</p>
 
     <form @submit.prevent="storeUsers.signin">
-        <input type="name" placeholder="Name" v-model="storeUsers.user.name" required>
+        <input type="name" placeholder="Name" v-model="displayName" required>
+        <input type="email" placeholder="Email" v-model="email" required>
+        <input type="password" placeholder="Password" v-model="password" required>
+        
+        
+        <!-- <input type="name" placeholder="Name" v-model="storeUsers.user.name" required>
         <input type="email" placeholder="Email" v-model="storeUsers.user.email" required>
-        <input type="password" placeholder="Password" v-model="storeUsers.user.password" required>
+        <input type="password" placeholder="Password" v-model="storeUsers.user.password" required> -->
         <button>SIGN IN</button>
     </form>
 </template>
@@ -12,6 +17,14 @@
 <script setup>
 // import { useDicesStore } from "@/stores/dices"
 import { useUsersStore } from "@/stores/users";
+import { ref } from "vue";
+
+// refs
+const displayName = ref('')
+const email = ref('')
+const password = ref('')
+
+// TODO: CALL FUNCTION WITH THE REFS AS PARAMETERS
 
 // const storeDices = useDicesStore()
 const storeUsers = useUsersStore()
