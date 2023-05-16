cead6122b2bb6b5979431e629186f00c589a4237
IvanSanczewski <isanczewski@gmail.com>
2023-05-16 17:29:00 +0300

    Implemented control of auth change on app onload, so once app is created user is not null even on refresh

--- src/main.js
+++ src/main.js
@@ -1,31 +1,48 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-
 import App from './App.vue'
 import router from './router'
 
+
+// import FIREBASE AUTH
+import { projectAuth } from './firebase/config'
+
+
+
+// import css 
 import './assets/CSS/main.css'
 
-// /* import the fontawesome core */
+
+// import the fontawesome core
 import { library } from '@fortawesome/fontawesome-svg-core'
-// /* import font awesome icon component */
+// import font awesome icon component
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// /* import specific icon libraries */
+// import specific icon libraries 
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
-// /* add icons to the library */
+// add icons to the library 
 library.add( fas, fab )
 
 
-const app = createApp(App)
+let app
+
+projectAuth.onAuthStateChanged(()=>{
+    if (!app) {
+        app = createApp(App)
+        
+        app.use(createPinia())
+        app.use(router)
+        app.component('font-awesome-icon', FontAwesomeIcon)
+        app.mount('#app')
+    }
+})
 
 
-app.use(createPinia())
-app.use(router)
+// const app = createApp(App)
 
-
-app.component('font-awesome-icon', FontAwesomeIcon)
-
-app.mount('#app')
+// app.use(createPinia())
+// app.use(router)
+// app.component('font-awesome-icon', FontAwesomeIcon)
+// app.mount('#app')
