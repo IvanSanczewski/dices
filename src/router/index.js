123474f3eac8b071988c8e5821d7f2d6cda220c2
IvanSanczewski <isanczewski@gmail.com>
2023-05-16 16:36:14 +0300

    Highscores route guarded when user is not logged

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { projectAuth } from '../firebase/config'
 
 import Home from '@/views/Home.vue'
 import DiceSum from '@/views/DiceSum.vue'
@@ -10,7 +11,17 @@ import Signin from '@/components/Signin.vue'
 import Login from '@/components/Login.vue'
 
 
+// auth guard
 
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser
+  console.log(user)
+  if (!user) { 
+    next({name: 'login'})
+  } else {
+    next()
+  }
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,7 +49,8 @@ const router = createRouter({
     {
       path: '/highScores',
       name: 'highScores',
-      component: HighScores
+      component: HighScores,
+      beforeEnter: requireAuth
     },
     {
       path: '/signin',
