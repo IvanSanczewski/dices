efe50ba86b72c2de8dfea9606fc0647a809a8073
IvanSanczewski <isanczewski@gmail.com>
2023-05-17 22:12:05 +0100

    Restayled Highscores page

--- src/views/HighScores.vue
+++ src/views/HighScores.vue
@@ -3,7 +3,7 @@
     <div v-for="game in storeScores.gameHighScore" :key="game.game" class="high__score__games">
       <p style="fontWeight: 900">{{ game.game }}</p>
       <div v-for="score in game.scores" :key="score.name">
-        <p>{{ score.name }} - {{ score.score }}</p>
+        <p style="color:darkolivegreen">{{ score.name }} - {{ score.score }}</p>
       </div>
     </div>  
 </template>
