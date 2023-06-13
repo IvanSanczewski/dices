aa181adbb964fec6e9059a8301ad8d96f8906bc4
IvanSanczewski <isanczewski@gmail.com>
2023-06-13 22:16:23 +0300

    Styled High Scores Page

--- src/views/HighScores.vue
+++ src/views/HighScores.vue
@@ -3,7 +3,7 @@
   <div v-for="game in storeScores.gameHighScores" :key="game.game" class="high__score__games">
     <h3>{{ game.game }}</h3>
     <div v-for="score in game.scores" :key="score.name">
-      <p>{{ score.name }} - {{ score.score }}</p>
+      <p> {{ score.name }} - {{ score.score }}</p>
     </div>
   </div>  
 </template>
@@ -12,7 +12,4 @@
 import { useScoresStore } from '@/stores/scores'
 
 const storeScores = useScoresStore()
-// fetchHighScores is perforemed on App load in order to have them available from the very beginning
-// storeScores.fetchHighScores()
-
 </script>
